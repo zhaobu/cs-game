2016f2e51fa88899eb83c3e2bfb30e58ee285ed4
liwei <chengyouwin10>
2019-08-06 13:16:24 +0800

    1:修改linux编译shell

--- game/build.sh
+++ game/build.sh
@@ -1,11 +1,17 @@
+set GOOS=linux
+
+go build -tags consul -o bin/linux/http cy/game/http
+
 go build -tags consul -o bin/linux/gate cy/game/gate
 
 go build -tags consul -o bin/linux/center cy/game/center
 
 go build -tags consul -o bin/linux/club cy/game/club
 
-# go build -tags consul -o bin/linux/ddz cy/game/logic/ddz
+REM go build -tags consul -o bin/linux/ddz cy/game/logic/ddz
 
 go build -o bin/linux/initdb cy/game/script/initdb
 
 go build -tags consul -o bin/linux/changshu cy/game/logic/changshu
+
+REM pause
