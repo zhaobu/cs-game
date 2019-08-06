6b7960e50c060f2c36d6a161f509e5b3acda1337
liwei <chengyouwin10>
2019-08-06 15:52:56 +0800

    1

--- game/build.sh
+++ game/build.sh
@@ -1,13 +1,13 @@
-set CGO_ENABLED=0
+set GOOS=linux GOARCH=amd64 CGO_ENABLED=0
 
-go build -tags netgo -tags consul -o bin/linux/http cy/game/http
+go build  -tags consul -o bin/linux/http cy/game/http
 
-GOOS=linux GOARCH=amd64 CGO_ENABLED=0  go build -tags netgo -tags consul -o bin/linux/gate cy/game/gate
+go build  -tags consul -o bin/linux/gate cy/game/gate
 
-go build -tags netgo -tags consul -o bin/linux/center cy/game/center
+go build  -tags consul -o bin/linux/center cy/game/center
 
-go build -tags netgo -tags consul -o bin/linux/club cy/game/club
+go build  -tags consul -o bin/linux/club cy/game/club
 
-go build -tags netgo -o bin/linux/initdb cy/game/script/initdb
+go build  -o bin/linux/initdb cy/game/script/initdb
 
-go build -tags netgo -tags consul -o bin/linux/changshu cy/game/logic/changshu
+go build  -tags consul -o bin/linux/changshu cy/game/logic/changshu
